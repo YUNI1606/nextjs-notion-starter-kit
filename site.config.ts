c932b68b5ad408eb1e9e5c007e23d31a352e5db5
YUNI1606 <166652170+YUNI1606@users.noreply.github.com>
2024-04-11 03:12:42 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,24 +2,24 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '9ddfe819f63e4a4c8a1ba28b2bd8d86a',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'IsYunilivngintherealworld',
+  domain: 'https://www.notion.so/IS-YUNI-LIVING-IN-THE-REAL-WORLD-9ddfe819f63e4a4c8a1ba28b2bd8d86a?showMoveTo=true&saveParent=true',
+  author: 'Yuni',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  Instagram: 'jiayunyang_',
+  github: 'nextjs-notion-starter-kit',
+  youtube:'Jiayun Yang',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
